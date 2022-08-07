a14dff53e6b7f3b0f9852be33827944d5fa794d0
Mike Koller <mkoller@eml.cc>
2022-08-07 11:03:35 -0600

    Create SQL statement and execute with snippetCreate handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -70,5 +70,18 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	// Create some variables holding dummy data
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	// Pass the data to the SnippetModel.Insert() method
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Redirect the user to the relevant snippet
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -22,7 +22,25 @@ type SnippetModel struct {
 
 // This will insert a new snippet into the database
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	// Write the SQL statement to be executed
+	stmt := `INSERT INTO snippets (title, content, created, expires) 
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use Exec() method to execute the statement
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use LastInsertId() to get ID of our newly inserted record in the snippets table
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The returned ID is int64
+	// Convert to int
+	return int(id), nil
 }
 
 // This will return a specific snippet based on ID
