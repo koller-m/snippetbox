d874ad971cecb06982ce80035aec99cca50dc9bd
Mike Koller <mkoller@eml.cc>
2022-08-14 10:55:37 -0600

    Use clean URLs and create custom 404 handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,22 +7,14 @@ import (
 	"strconv"
 
 	"github.com/koller-m/snippetbox/internal/models"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // Define home handler function
 // Writes a byte slice containing
 // "Hello from Snippetbox" as the response body
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// Check if the current request matches "/"
-	// If not, use http.NotFound() to send 404 response
-	// Return from the handler
-	// If not, the handler would keep executing
-	// And also write "Hello from Snippetbox"
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -39,10 +31,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 // Add snippetView handler function
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
 	// Extract the value of the id param from query string
 	// Convert it to int using strconv.Atoi()
 	// If it can't be converted or value is less than 1, return 404
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -67,15 +61,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
 
-// Add snippetCreate handler function
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check if the request is POST
-	if r.Method != "POST" {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	// Create some variables holding dummy data
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
@@ -88,6 +78,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Redirect the user to the relevant snippet
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	// Update redirect path to use clean URL format
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,22 +3,31 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	// Init the router
+	router := httprouter.New()
 
+	// Create handler function that wraps notFound() helper
+	// Then assign it as a custom handler for 404 Not Found responses
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
+	// Update route pattern for static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// Create middleware chain which will be used for every request
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	// Return middleware chain followed by servemux
-	return standard.Then(mux)
+	return standard.Then(router)
 }
